0166b248d0469f20da0fca2506791bdcf0a9994e
Алексей <filippovalex1985@yandex.ru>
2023-12-26 16:31:09 +0300

    new file

---
+++ src/main/java/org/example/Connect.java
@@ -0,0 +1,27 @@
+package org.example;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connect {
+    public static void connect() {
+        Connection conn = null;
+        try {
+            String url = "jdbc:sqlite:sq.db";
+            conn = DriverManager.getConnection(url);
+            System.out.println("Connection to SQLite");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+}
---
+++ src/main/java/org/example/Select.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import java.sql.*;
+
+public class Select {
+    private Connection connect() {
+        String url = "jdbc:sqlite:sq.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }return conn;
+    }
+
+    public void selectAll() {
+        String sql = "select employee_id,first_name,last_name from Users";
+        try (Connection conn = this.connect();
+             Statement statement = conn.createStatement();
+             ResultSet rs = statement.executeQuery(sql)) {
+            System.out.println("employee_id " + "first_name " + " last_name");
+            System.out.println("----------- " + "---------- " + " ---------");
+            while (rs.next()) {
+                System.out.println(rs.getInt("employee_id") +
+                        "\t\t\t" + firstUpperCase( rs.getString("first_name")) +
+                        "\t\t" + firstUpperCase(rs.getString("last_name")));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public String firstUpperCase(String word){
+        if(word == null || word.isEmpty()) return "";
+        return word.substring(0, 1).toUpperCase() + word.substring(1);
+    }
+}
